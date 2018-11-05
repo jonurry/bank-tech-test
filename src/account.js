1816428f6165156784fd48e0e6a4490e458c225b
Jon Urry <jon@urry.me>
2018-11-05 17:26:56 +0000

    account receives transaction class
    allow transaction class to be passed to account constructor
    transaction create and stored when accounbt credited
    use mock transactions when testing account

--- src/account.js
+++ src/account.js
@@ -1,13 +1,15 @@
 import Transaction from './transaction'
 
 export default class Account {
-  constructor(balance = 0, transactionClass = Transaction) {
-    this.balance = balance
+  constructor(transactionClass = Transaction) {
+    this.balance = 0
     this.transactions = []
     this.transactionClass = transactionClass
   }
   credit(amount) {
     this.balance += amount
+    let transaction = new this.transactionClass(amount)
+    this.transactions.push(transaction)
   }
   debit(amount) {
     this.balance -= amount
--- src/account.test.js
+++ src/account.test.js
@@ -1,5 +1,6 @@
 import Account from './account'
 import Transaction from './transaction'
+jest.mock('./transaction')
 
 describe('account', () => {
   describe('constructor', () => {
@@ -19,27 +20,29 @@ describe('account', () => {
       })
     })
     describe('override defaults', () => {
-      test('account has a set balance', () => {
-        let account = new Account(100)
-        expect(account.balance).toBe(100)
-      })
-      // test('account has set transactions', () => {
-      //   let transation = {date: Date.now(), amount: 20}
-      //   let account = new Account(0, [transation])
-      //   expect(account.transactions).toEqual([transation])
-      // })
       test('account has a specific transaction maker', () => {
         let mock = jest.fn()
-        let account = new Account(100, mock)
+        let account = new Account(mock)
         expect(account.transactionClass).toBe(mock)
       })
     })
   })
   describe('credit', () => {
-    test('user can credit account with money', () => {
-      let account = new Account()
+    let account
+    beforeEach(() => {
+      // Clear all instances and calls to constructor and all methods:
+      Transaction.mockClear()
+      account = new Account(Transaction)
       account.credit(50)
+    })
+    test('should credit account with money', () => {
       expect(account.balance).toBe(50)
+    })
+    test('should create a transaction', () => {
+      expect(Transaction).toHaveBeenCalledTimes(1)
+    })
+    test('should store one item in transactions[]', () => {
+      expect(account.transactions).toHaveLength(1)
     })
     // describe('with a date', () => {})
     // describe('without a date', () => {
@@ -52,9 +55,10 @@ describe('account', () => {
   })
   describe('debit', () => {
     test('user can debit money from an account', () => {
-      let account = new Account(100)
+      let mock = jest.fn()
+      let account = new Account(mock)
       account.debit(50)
-      expect(account.balance).toBe(50)
+      expect(account.balance).toBe(-50)
     })
   })
 })
