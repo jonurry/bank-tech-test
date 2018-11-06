e5db6dc528a8c9d50beb1d295de1d06de53613e7
Jon Urry <jon@urry.me>
2018-11-06 09:33:11 +0000

    initial statement test
    set up statement test and return hard coded expectation

---
+++ src/statement.test.js
@@ -0,0 +1,29 @@
+import Statement from './statement'
+
+describe('statement', () => {
+  describe('print', () => {
+    test('should output transactions', () => {
+      let statement = new Statement()
+      let transactions = [
+        {
+          amount: 1000,
+          date: Date('2012/01/10'),
+        },
+        {
+          amount: 2000,
+          date: Date('2012/01/13'),
+        },
+        {
+          amount: -500,
+          date: Date('2012/01/14'),
+        },
+      ]
+      expect(statement.print(transactions)).toEqual(
+        `date || credit || debit || balance
+14/01/2012 || || 500.00 || 2500.00
+13/01/2012 || 2000.00 || || 3000.00
+10/01/2012 || 1000.00 || || 1000.00`,
+      )
+    })
+  })
+})
