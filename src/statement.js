18efbba090b6fa6091e891517b4001e167bb9f2a
Jon Urry <jon@urry.me>
2018-11-06 11:08:15 +0000

    refactor print statement
    create private functions and refactor statement printing code.

--- src/statement.js
+++ src/statement.js
@@ -3,26 +3,44 @@ export default class Statement {
   print(transactions) {
     let statement = []
     let balance = 0
-    let sortedTransactions = transactions.sort((a, b) => {
-      if (a.date < b.date) return -1
-      if (a.date > b.date) return 1
-      return 0
-    })
-    sortedTransactions.forEach(transaction => {
-      let credit = ' '
-      let debit = ' '
-      date = transaction.date
-      let date = `${date.getDate()}/${String(date.getMonth() + 1).padStart(
-        2,
-        '0',
-      )}/${date.getFullYear()}`
-      transaction.amount >= 0
-        ? (credit = ` ${transaction.amount.toFixed(2)} `)
-        : (debit = ` ${(-transaction.amount).toFixed(2)} `)
+    sortTransactionsByDate(transactions).forEach(transaction => {
+      let credit = formatCredit(transaction.amount)
+      let debit = formatDebit(transaction.amount)
+      let date = formatDate(transaction.date)
       balance += transaction.amount
-      statement.push(`\n${date} ||${credit}||${debit}|| ${balance.toFixed(2)}`)
+      statement.unshift(
+        `\n${date} ||${credit}||${debit}|| ${formatBalance(balance)}`,
+      )
     })
-    statement.push('date || credit || debit || balance')
-    return statement.reverse().join('')
+    statement.unshift('date || credit || debit || balance')
+    return statement.join('')
   }
+}
+
+const formatBalance = balance => {
+  return balance.toFixed(2)
+}
+
+const formatCredit = amount => {
+  return amount >= 0 ? ` ${amount.toFixed(2)} ` : ' '
+}
+
+const formatDebit = amount => {
+  return amount < 0 ? ` ${(-amount).toFixed(2)} ` : ' '
+}
+
+const formatDate = date => {
+  return `${date.getDate()}/${String(date.getMonth() + 1).padStart(
+    2,
+    '0',
+  )}/${date.getFullYear()}`
+}
+
+const sortTransactionsByDate = transactions => {
+  let sortedTransactions = transactions.sort((a, b) => {
+    if (a.date < b.date) return -1
+    if (a.date > b.date) return 1
+    return 0
+  })
+  return sortedTransactions
 }
